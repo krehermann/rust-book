b13cfa4818364f9cdf4fe0862970b42f1953e6b9
Keith Rehermann <krehermann@nvidia.com>
2022-03-04 16:52:38 -0700

    Functional Programming: Closures
    First pass at wrapping a closure in a struct.
    
    Cacher wraps a closure and store the value for future reference.

--- ch13/src/lib.rs
+++ ch13/src/lib.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use std::thread;
 use std::time::Duration;
 
@@ -30,12 +31,43 @@ fn generate_workout(intensity: u32,random_number:u32) {
     }
 }
 
+/// Cacher is struct that wraps a closure and caches the evaluation
+struct Cacher<T,U>
+where T: Fn(U) -> U,
+{
+    calculation:T,
+    value:Option<U>,
+}
+
+
+impl<T,U:Copy> Cacher<T,U> where T:Fn(U)->U {
+    fn new(calculation:T) ->Cacher<T,U> {
+        Cacher { calculation: calculation, value: None }
+    }
+
+    fn value(&mut self, arg: U) -> U {
+        match self.value{
+            // cache first result forevs
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-   
+    fn cacher() {
+        let closure = |i: u32| {
+            i*2
+        };
+        let mut c = Cacher::new(closure);
+        let v = c.value(4);
+        assert_eq!(v, 8);
     }
 }
